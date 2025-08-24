1df516f81f8e70cfc6c2b022a882570327d2997e
RomanGRS <ggr.s-74@mail.ru>
2025-08-24 15:36:27 +0300

    Update index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,58 @@
+export type ProductStatus = 'wait' | 'active' | 'closed';
+
+interface IAuction {
+	status: ProductStatus;
+	datetime: string;
+	price: number;
+	minPrice: number;
+	history?: number[];
+}
+
+export interface IProductItem {
+	id: string;
+	title: string;
+	about: string;
+	description?: string;
+	image: string;
+}
+
+export type IProduct = IProductItem & IAuction;
+export type ProductUpdate = Pick<
+	IProduct,
+	'id' | 'status' | 'price' | 'history'
+>;
+
+export type IBasketItem = Pick<IProduct, 'id' | 'title' | 'price'> & {
+	isMyBid: boolean;
+};
+
+export interface IAppState {
+	catalog: IProductItem[];
+	preview: string;
+	basket: IBasketItem[]; // Уточнение типа элементов корзины
+	order: IOrder;
+	total: number; // Изменение типа на number
+	loading: boolean;
+}
+
+export interface IProductsList {
+	products: IProductItem[];
+}
+
+export interface IOrderForm {
+	payment?: string;
+	address?: string;
+	phone?: string;
+	email?: string;
+}
+
+export interface IOrder extends IOrderForm {
+	items: string[];
+	total?: string | number;
+}
+
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
+
+export interface IOrderResult {
+	id: string;
+}
